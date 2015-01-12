ccc608c87ba33fb2cb24c5973ab7ea08c94cae13
Rafael Medina <rafamedgar@gmail.com>
2015-01-12 18:14:46 +0100

    Added resource gathering

--- agents/sysinfo/sysinfo.py
+++ agents/sysinfo/sysinfo.py
@@ -25,3 +25,111 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.)
 
+import datetime
+import os
+import psutil
+import zoe
+from os import environ as env
+from os.path import join as path
+from zoe.deco import *
+
+
+@Agent(name="sysinfo")
+class Sysinfo:
+
+    def gather_cpu(self):
+        """ Gather information on system CPU.
+
+            Obtains usage percentage for each CPU present for user,
+            system and idle.
+        """
+        result = {}
+        cpu_info = psutil.cpu_times_percent(percpu=True)
+
+        for index, cpu in enumerate(cpu_info):
+            result["cpu" + str(index)] = {
+                "user": cpu.user,
+                "system": cpu.system,
+                "idle": cpu.idle
+            }
+
+        return result
+
+    def gather_disk(self):
+        """ Gather information on system disks.
+
+            Obtains mounted disk partitions and their usage statistics.
+        """
+        result = {}
+        partitions = psutil.disk_partitions()
+
+        for partition in partitions:
+            part_usage = psutil.disk_usage(partition.mountpoint)
+
+        result[partition.device] = {
+            "mountpoint": partition.mountpoint,
+            "fstype": partition.fstype,
+            "opts": partition.opts,
+            "usage": {
+                "total": part_usage.total,
+                "used": part_usage.used,
+                "free": part_usage.free,
+                "percentage": part_usage.percent
+            }
+        }
+
+        return result
+
+    def gather_memory(self):
+        """ Gather information on system memory.
+
+            Obtains physical RAM and swap statistics.
+        """
+        result = {}
+        mem_info = psutil.virtual_memory()
+        swap_info = psutil.swap_memory()
+
+        # RAM
+        result["ram"] = {
+            "total": mem_info.total,
+            "free": mem_info.available,
+            "used": mem_info.used,
+            "percentage": mem_info.percent
+        }
+
+        # SWAP
+        result["swap"] = {
+            "total": swap_info.total,
+            "free": swap_info.free,
+            "used": swap_info.used,
+            "percentage": swap_info.percent
+        }
+
+        return result
+
+    def gather_proc(self):
+        """ Gather information on running processes.
+
+            Obtains pid, name, executable, user running the process,
+            status of the process and memory usage.
+        """
+        result = {}
+
+        for proc in psutil.process_iter():
+            try:
+                process = psutil.Process(proc.pid)
+
+                proc_data = process.as_dict(
+                    attrs=["name", "exe", "username"])
+                
+                mem_info = process.memory_info()
+                proc_data["memory"] = {}
+                proc_data["memory"]["resident"] = mem_info.rss
+                proc_data["memory"]["virtual"] = mem_info.vms
+
+                result[proc.pid] = proc_data
+
+            except psutil.NoSuchProcess:
+                continue
+
+        return result
